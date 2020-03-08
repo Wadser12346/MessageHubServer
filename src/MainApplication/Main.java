435bf746042c97676809b167d6c6044c7066b0d4
jwidjaja0 <jwidjaja0@saddleback.edu>
2020-03-07 17:10:53 -0800

    loading ui, throws exception can't find file

--- src/MainApplication/Main.java
+++ src/MainApplication/Main.java
@@ -1,7 +1,21 @@
 package MainApplication;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/Server.fxml"));
+        primaryStage.setTitle("SERVER");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
