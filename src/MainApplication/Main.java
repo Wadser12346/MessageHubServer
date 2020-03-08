462de921b348807895da6eff0f745b9230b05604
jwidjaja0 <jwidjaja0@saddleback.edu>
2020-03-08 15:55:47 -0700

    basic server running, can connect with client

--- src/MainApplication/Main.java
+++ src/MainApplication/Main.java
@@ -6,18 +6,111 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+
 public class Main extends Application {
 
+    private int clientNo = 0;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/Server.fxml"));
-        primaryStage.setTitle("SERVER");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-        System.out.println("Hello");
+//        Parent root = FXMLLoader.load(getClass().getResource("FXML/Server.fxml"));
+//        primaryStage.setTitle("SERVER");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+
+        new Thread( () -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8000);
+                System.out.println("MultiThreaded server started at " + new Date() + '\n');
+
+                while(true){
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Starting thread for client " + clientNo + " at " + new Date() + '\n');
+
+                    InetAddress inetAddress = socket.getInetAddress();
+                    System.out.println("Client " + clientNo  + "'s host name is " + inetAddress.getHostName() + "\n");
+                    System.out.println("Client " + clientNo + "'s host address is " + inetAddress.getHostAddress() + "\n");
+
+                    Thread thread1 = new Thread(new HandleAClient(socket, inetAddress));
+                    thread1.start();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
     }
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
 
+
+//        Thread thread = new Thread(new StartServerTask());
+
+
+
+    }
+
+    static class HandleAClient implements Runnable{
+        private Socket socket;
+        private InetAddress inetAddress;
+
+        public HandleAClient(Socket socket, InetAddress inetAddress){
+            this.socket = socket;
+            this.inetAddress = inetAddress;
+        }
+
+        @Override
+        public void run() {
+            try {
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while(true){
+                    String message = inputFromClient.readUTF();
+                    System.out.println(inetAddress.getHostName() + ": " + message);
+                    outputToClient.writeUTF(message); //send message back to client
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class StartServerTask implements Runnable{
+        int clientNo = 0;
+
+        @Override
+        public void run() {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8000);
+                System.out.println("MultiThreaded server started at " + new Date() + '\n');
+
+                while(true){
+                    Socket socket = serverSocket.accept();
+                    clientNo++;
+                    System.out.println("Starting thread for client " + clientNo + " at " + new Date() + '\n');
+
+                    InetAddress inetAddress = socket.getInetAddress();
+                    System.out.println("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + "\n");
+                    System.out.println("Client " + clientNo + "'s host name is " + inetAddress.getHostAddress() + "\n");
+
+//                    Thread thread1 = new Thread(new HandleAClient(socket, inetAddress));
+//                    thread1.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
