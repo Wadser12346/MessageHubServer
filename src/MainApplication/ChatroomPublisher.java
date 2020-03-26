79abc23e1014d5c9c3a7747c697a1469a0e90af7
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-03-26 01:40:41 -0700

    added implementation of chatroomPublisher

--- src/MainApplication/ChatroomPublisher.java
+++ src/MainApplication/ChatroomPublisher.java
@@ -1,20 +1,63 @@
 package MainApplication;
 
+import CS4B.Messages.ChatMessage;
 import CS4B.Messages.Packet;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class ChatroomPublisher implements Runnable {
     private String chatroomName;
-    private List<ClientConnection> subscribedClients;
+    private List<ClientConnection> subscribedClientList;
     private BlockingQueue<Packet> chatroomMessageQueue;
 
+    public ChatroomPublisher(String chatroomName, List<ClientConnection> subscribedClientList) {
+        this.chatroomName = chatroomName;
+        this.subscribedClientList = subscribedClientList;
 
+        chatroomMessageQueue = new ArrayBlockingQueue<>(500);
+    }
 
+    public void addClientToRoom(ClientConnection client){
+        subscribedClientList.add(client);
+    }
+
+    //TODO: Move this function to inside packet so server can print necessary info easily, or update toString();
+    private String trimPacketMessage(Packet packet){
+        if(packet.getMessage() instanceof ChatMessage){
+            ChatMessage cm = (ChatMessage)packet.getMessage();
+            StringBuilder stringBuilder = new StringBuilder("");
+            stringBuilder.append(packet.getUser() + ": ");
+            stringBuilder.append(cm.getStringMessage());
+
+            if(cm.hasPictureMessage()){
+                stringBuilder.append(" + Image");
+            }
+
+            return stringBuilder.toString();
+        }
+
+        return packet.toString();
+    }
 
     @Override
     public void run() {
+        while(true){
+            try {
+                Packet toPublish = chatroomMessageQueue.take();
+                String publishMessage = chatroomName + " publish: " + trimPacketMessage(toPublish);
+                System.out.println(publishMessage);
 
+                for (ClientConnection c :
+                        subscribedClientList) {
+                    c.getObjectOutputStream().writeObject(toPublish);
+                }
+                
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
